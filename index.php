719f31923dc6e7d4d8e2077cf07136e412c9bf22
febriansahjeri <febriansahjeri56@gmail.com>
2026-01-03 21:57:37 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manajemen Siswa</title>
+    <link rel="stylesheet" href="assets/style.css"> 
+</head>
+<body>
+    <div class="container">
+        <h1>Selamat Datang di Sistem Manajemen Siswa</h1>
+        
+        <a href="dashboard/index.php" class="start-now-btn">Buka Dashboard</a>
+        
+        <br>
+        <a href="auth/login.php">Login</a>
+    </div>
+</body>
+</html>
